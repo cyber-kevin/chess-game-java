65456964a4a36ad86bef4419547738bca1dcee70
Kevin Ferreira <devkevferreira@gmail.com>
2022-12-19 12:08:23 -0300

    Refactoring: Modifying Color enumand UI Class

--- src/chess/Color.java
+++ src/chess/Color.java
@@ -1,6 +1,27 @@
 package chess;
 
 public enum Color {
-    BLACK,
-    WHITE;
+
+    BLACK("\u001B[30m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    CYAN("\u001B[36m"),
+    WHITE("\u001B[37m");
+
+    private String color;
+
+    private Color(String color) {
+        this.color = color;
+    }
+
+    public String paint() {
+        return color;
+    }
+
+    public String reset() {
+        return "\u001B[0m";
+    }
+
+
 }
